560203f06f64ce07333e992cf3aa94eb84f48dd7
Mrinal Gupta <tabil31649@eqvox.com>
2025-06-18 14:19:43 +0000

    test_app:: Minor app update Data Query 'data_1' added. Screen 'Screen 1' modified 'querybuilder' control deleted in 'Screen 1' screen. 'querybuilder' control added in 'Screen 1' screen.

---
+++ App/test_app/data_query/data_1/query.js
@@ -0,0 +1,35 @@
+[
+    {
+        "label": "Status",
+        "value": "status",
+        "enum": [
+            {
+                "label": "Approved",
+                "value": "approved"
+            },
+            {
+                "label": "Rejected",
+                "value": "rejected"
+            },
+            {
+                "label": "Pending",
+                "value": "pending"
+            }
+        ]
+    },
+    {
+        "label": "User Name",
+        "value": "username",
+        "type": "text"
+    },
+    {
+        "label": "Email",
+        "value": "email",
+        "type": "text"
+    },
+    {
+        "label": "Age",
+        "value": "age",
+        "type": "number"
+    }
+]
