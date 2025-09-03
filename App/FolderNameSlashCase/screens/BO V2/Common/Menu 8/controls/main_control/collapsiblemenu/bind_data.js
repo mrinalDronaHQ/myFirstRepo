5dc29e5c7ff9f427cdabebaa552381ffd60e23fc
Mrinal Gupta <tabil31649@eqvox.com>
2025-09-03 20:22:02 +0000

    FolderNameSlashCase:: Minor app update Screen 'undefined' added. Screen 'Menu 11' added. Screen 'Screen 10' added. Screen 'Popup 5' modified Screen 'Screen 1' modified Screen 'Screen 2' modified Screen 'Screen 3' modified Screen 'Screen 4' modified Screen 'Screen 6' modified Screen 'Screen 7' modified

---
+++ App/FolderNameSlashCase/screens/BO V2/Common/Menu 8/controls/main_control/collapsiblemenu/bind_data.js
@@ -0,0 +1,72 @@
+[
+    {
+        "label": "Home",
+        "value": "home",
+        "icon": "icon-home",
+        "active": true
+    },
+    {
+        "label": "Job",
+        "value": "job",
+        "icon": "icon-briefcase",
+        "children": [
+            {
+                "label": "Analyst",
+                "children": [
+                    {
+                        "label": "Data Analyst",
+                        "value": "data_analyst",
+                        "icon": "icon-cloud-upload"
+                    },
+                    {
+                        "label": "Project Analyst",
+                        "value": "project_analyst",
+                        "icon": "icon-grid",
+                        "image": "",
+                        "active": true,
+                        "tag": {
+                            "color": "#FF00F5",
+                            "value": "new"
+                        }
+                    },
+                    {
+                        "label": "Program Analyst",
+                        "value": "program_analyst",
+                        "icon": "icon-vector",
+                        "active": true
+                    }
+                ],
+                "value": "analyst",
+                "icon": "icon-graph"
+            }
+        ]
+    },
+    {
+        "label": "Apply",
+        "value": "apply",
+        "icon": "icon-cursor"
+    },
+    {
+        "label": "Explore",
+        "value": "explore",
+        "icon": "icon-globe",
+        "image": "",
+        "children": [
+            {
+                "label": "Sevices",
+                "value": "sevices",
+                "icon": "icon-settings"
+            }
+        ]
+    },
+    {
+        "label": "User",
+        "value": "user",
+        "icon": "icon-user",
+        "image": "",
+        "tag": {
+            "color": "var(--DHQ-primary-color)",
+            "value": "3"
+        }
+    }
+]
